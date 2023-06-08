f55cad80edcc819306d9b1be3e82a55575850320
sri.tikkireddy <sri.tikkireddy@databricks.com>
2023-06-08 11:43:07 -0400

    fixed setup.py language

--- python/setup.py
+++ python/setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='databricks-reusable-job-clusters',
     version='1.0.0',
-    author='Your Name',
-    author_email='your@email.com',
-    description='A dummy package for demonstration purposes',
+    author='Sri Tikkireddy',
+    author_email='sri.tikkireddy@databricks.com',
+    description='A package for building airflow operators to reuse Jobs Clusters',
     packages=find_packages(exclude=['tests']),
     package_data={'': ['infinite_loop_notebook.template']},
     install_requires=[
@@ -22,12 +22,11 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
-    keywords='dummy package',
+    keywords='Databricks Clusters',
 )
