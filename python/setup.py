0b1e9f4503c68eccc756960081bdf017c302a9dc
sri.tikkireddy <sri.tikkireddy@databricks.com>
2023-06-14 12:21:24 -0400

    added licenses and updated setup.py to include Juan and updated README.md

--- python/setup.py
+++ python/setup.py
@@ -2,9 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='databricks-reusable-job-clusters',
-    version='1.0.0',
-    author='Sri Tikkireddy',
-    author_email='sri.tikkireddy@databricks.com',
+    author='Sri Tikkireddy, Juan Lamadrid',
+    author_email='sri.tikkireddy@databricks.com, juan.lamadrid@databricks.com',
     description='A package for building airflow operators to reuse Jobs Clusters',
     packages=find_packages(exclude=['tests']),
     package_data={'': ['infinite_loop_notebook.template']},
@@ -18,13 +17,14 @@ setup(
         "aiohttp>=3.6.3, <4",
         "requests>=2.27,<3",
     ],
+    license_files=('LICENSE',),
     extras_require={
-            'dev': [
-                'pytest',
-                'pytest-cov',
-                'pytest-asyncio',
-            ]
-        },
+        'dev': [
+            'pytest',
+            'pytest-cov',
+            'pytest-asyncio',
+        ]
+    },
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
